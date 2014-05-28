97136b8c71fc890f3f6b02a437c1958943996e6d
Sam Gladstone <gladston@umich.edu>
2014-05-28 15:57:03 -0400

    Actors list and indidivual actors pages fully functional

--- server.rb
+++ server.rb
@@ -13,14 +13,52 @@ def db_connection
   end
 end
 
-get '/' do
-  @title = "Launch Academy Movies"
-  @page_title = "Launch Academy Movie List"
-  @actors =
+
     db_connection do |conn|
       conn.exec('SELECT actors.name FROM actors ORDER BY actors.name')
     end
+
+
+
+def find_actor_name(id)
+  actor_id = db_connection do |conn|
+      conn.exec("SELECT actors.id, actors.name FROM actors WHERE actors.id = #{id}")
+    end
+  actor_id.to_a.first["name"]
+end
+
+def find_actor_movies(id)
+  actor_id = db_connection do |conn|
+    conn.exec("SELECT movies.title, cast_members.character FROM movies
+              JOIN cast_members ON movies.id = cast_members.movie_id
+              WHERE cast_members.actor_id = #{id}")
+  end
+  actor_id
+end
+
+# movies =
+#     db_connection do |conn|
+#       conn.exec('SELECT movies.title, cast_members.character FROM movies
+#                 JOIN cast_members ON movies.id = cast_members.movie_id
+#                 WHERE cast_members.actor_id = 60') # replace 60 with params
+#     end
+
+get '/' do
   erb :index
 end
 
+get '/actors' do
+  @title = "Launch Academy Movies"
+  @page_title = "All Actors"
+  @actors = db_connection do |conn|
+              conn.exec('SELECT actors.name, actors.id FROM actors ORDER BY actors.name LIMIT 10')
+            end
+  erb :'actors/actors'
+end
+
+get '/actors/:id' do
+  @page_title = find_actor_name(params[:id])
+  @actor_movies = find_actor_movies(params[:id])
+  erb :'actors/show.html'
+end
 
